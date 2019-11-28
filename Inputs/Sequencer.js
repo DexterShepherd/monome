d5d679eb67b919dbe482bc21a5e09e173da8fcd1
dexter <dexterjshepherd@gmail.com>
2019-11-27 20:50:14 -0800

    adding pattern chaining and breaking a bunch of more important things

--- Inputs/Sequencer.js
+++ Inputs/Sequencer.js
@@ -2,6 +2,7 @@ const { onTick } = require('../Transport')
 const { set } = require('../Grid')
 const { bus, subscribe } = require('../EventBus')
 const { output, send } = require('../Midi')
+const { cloneDeep } = require('lodash')
 
 class Sequencer {
   constructor(startRow, rowCount) {
@@ -9,21 +10,28 @@ class Sequencer {
     this.startRow = startRow
     this.steps = rowCount * 8
     this.isFocused = false
+    this.stage = 0
+    this.stageCount = 1
+    this.focusStage = 0
+    this.euclidPattern = []
 
     this.pattern = Array(this.steps)
       .fill()
-      .map(_ => 0)
+      .map(_ => ({
+        val: 0
+      }))
 
-    this.inputPattern = this.pattern.slice()
+    this.inputPattern = cloneDeep(this.pattern)
 
     bus.on('key', ({ x, y, s }) => {
       if (this.isFocused) {
         if (y >= this.startRow && y < this.startRow + this.rowCount) {
           if (s == 1) {
-            const index = x + (y - this.startRow) * 8
-            this.pattern[index] = Number(!this.pattern[index])
-            this.inputPattern[index] = this.pattern[index]
-            set(x, y, this.pattern[index])
+            const index = this.gridToPattern(x, y)
+            console.log(index)
+            this.pattern[index].val = Number(!this.pattern[index].val)
+            this.inputPattern[index].val = this.pattern[index].val
+            set(x, y, this.pattern[index].val)
           }
         }
       }
@@ -31,43 +39,50 @@ class Sequencer {
 
     this.lastTickPos = null
 
-    onTick(tick => {
-      const pos = tick % this.steps
-      const x = tick % 8
-      const y = this.startRow + Math.floor(pos / 8)
+    bus.on('tick', tick => {
+      setTimeout(() => {
+        const pos = (tick % this.steps) + this.stage * this.steps
+        const { x, y } = this.patternToGrid(pos)
 
-      if (this.isFocused) {
-        if (this.lastTickPos !== null) {
-          set(
-            this.lastTickPos.x,
-            this.lastTickPos.y,
-            this.pattern[this.gridToPattern(this.lastTickPos.x, this.lastTickPos.y)]
-          )
+        if (this.isFocused && this.focusStage == this.stage) {
+          if (this.lastTickPos !== null) {
+            set(
+              this.lastTickPos.x,
+              this.lastTickPos.y,
+              this.pattern[this.gridToPattern(this.lastTickPos.x, this.lastTickPos.y)].val
+            )
+          }
+          set(x, this.startRow + Math.floor((pos % 16) / 8), 1)
         }
-        set(x, this.startRow + Math.floor(pos / 8), 1)
-      }
-      if (this.pattern[pos] == 1) {
-        send({ note: this.trackIndex + 36, channel: 1, velocity: 127 }, 100)
-      }
-      this.lastTickPos = { x, y }
+        if (this.pattern[pos].val == 1) {
+          send({ note: this.trackIndex + 36, channel: 1, velocity: 127 }, 100)
+        }
+        this.lastTickPos = { x, y }
+      }, 5)
     })
   }
   patternToGrid(index) {
     const x = index % 8
-    const y = Math.floor(index / 8) + this.startRow
+    const y = Math.floor((index % 16) / 8) + this.startRow
     return { x, y }
   }
   gridToPattern(x, y) {
-    return x + (y - this.startRow) * 8
+    return (x % 8) + (y - this.startRow) * 8 + this.steps * this.focusStage
   }
   writePatternToGrid() {
-    this.pattern.forEach((val, i) => {
-      const { x, y } = this.patternToGrid(i)
-      set(x, y, val)
-    })
+    cloneDeep(this.pattern)
+      .slice(this.focusStage * this.steps, (this.focusStage + 1) * this.steps)
+      .forEach(({ val }, i) => {
+        const { x, y } = this.patternToGrid(i)
+        set(x, y, val)
+      })
   }
+
   addPattern(pattern) {
-    this.pattern = this.inputPattern.map((val, i) => Math.max(val, pattern[i]))
+    // this.pattern = this.inputPattern.map(({ val }, i) => ({
+    //   val: Math.max(val, pattern[i % 16])
+    //   fromEuclid: true
+    // }))
     this.writePatternToGrid()
   }
 
@@ -81,6 +96,32 @@ class Sequencer {
     subscribe(instance, 'track', ({ focus }) => {
       this.writePatternToGrid()
       this.isFocused = focus
+    })
+    subscribe(instance, 'stageUpdate', ({ stageCount, stage, focusStage }) => {
+      const lastStageCount = this.stageCount
+      const lastStage = this.stage
+      const lastFocusState = this.focusStage
+
+      this.stage = stage
+      this.stageCount = stageCount
+      if (focusStage !== undefined) {
+        this.focusStage = focusStage
+        this.writePatternToGrid()
+      }
+
+      if (lastStage !== this.stage && this.isFocused) {
+        this.writePatternToGrid()
+      }
+
+      if (this.stageCount > lastStageCount) {
+        while (this.pattern.length < this.stageCount * this.steps) {
+          this.pattern = [...cloneDeep(this.pattern), ...cloneDeep(this.pattern)]
+        }
+        this.pattern = cloneDeep(this.pattern).slice(0, this.stageCount * this.steps)
+      } else {
+        this.pattern = cloneDeep(this.pattern).slice(0, this.stageCount * this.steps)
+      }
+      this.inputPattern = cloneDeep(this.pattern)
     })
   }
 }
--- Transport.js
+++ Transport.js
@@ -1,22 +1,24 @@
 const accurateInterval = require('accurate-interval')
 
-
-const tickRate = 100
+const { bus } = require('./EventBus')
+const tickRate = 111.94
 let tick = 0
 const tickCallbacks = []
 
-const onTick = (callback) => {
+const onTick = callback => {
   tickCallbacks.push(callback)
 }
 
 accurateInterval(() => {
-  tickCallbacks.forEach((cb) => {
+  tickCallbacks.forEach(cb => {
     cb(tick)
   })
+  bus.emit('tick', tick)
   tick += 1
 }, tickRate)
 
 module.exports = {
   tick,
+  tickRate,
   onTick
 }
